c41b291553bb7e182b00fc77a673cc86d494364e
connor <cmcummings2003@gmail.com>
2019-03-14 14:51:37 -0500

    Added config

---
+++ config.py
@@ -0,0 +1,16 @@
+# config.py
+# Stores program-wide configurations
+
+
+SIZE = WIDTH, HEIGHT = 1280, 720
+
+CANVAS_SIZE = CANVAS_WIDTH, CANVAS_HEIGHT = WIDTH*2/3, HEIGHT
+CANVAS_FPS = 60
+
+DEFAULT_MAP = [[0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0],
+               [2, 0, 0, 1, 0, 3, 0],
+               [0, 0, 0, 1, 0, 0, 0],
+               [0, 0, 0, 1, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0],]
