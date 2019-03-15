9a624d7aae6b417cc3e394d5711b0e268e04b490
connor <cmcummings2003@gmail.com>
2019-03-15 13:23:24 -0500

    Update visualization

--- astar.py
+++ astar.py
@@ -27,14 +27,14 @@ class AStar:
         self.start_node.h = get_distance_to_end(self.start_node, self.end)
 
         # Initialize the open/closed lists
-        self.open = self.get_nodes_around(self.start)
+        self.open = self.get_nodes_around(self.start_node)
         self.closed = [self.start_node]
 
-        self.advance()
-
+        # Initialize the result variables
+        self.end_path = None
 
     def read_level_map(self, level_map):
-        """Reads a 2D array and returns the start and end nodes.
+        """Reads a 2D list and returns the start and end nodes.
            Raises an error if there are no start or end nodes."""
         start = None
         end = None
@@ -63,6 +63,9 @@ class AStar:
 
     def advance(self):
         """Advances the search."""
+        # Check if there is an open node
+        if len(self.open) == 0:
+            print("No path available")
         # Get the node on the open list which has the lowest score (S).
         S = self.open[0]
         for node in self.open:
@@ -71,31 +74,37 @@ class AStar:
         # Remove S from the open list and add it to the closed list
         self.open.remove(S)
         self.closed.append(S)
-        # For each node T in S’s walkable adjacent tiles: 
+        # For each node T in S's walkable adjacent tiles:
         for T in self.get_nodes_around(S):
-            T_node = Node(T[0], T[1], parent=S)
-            g, h = self.calculate_score(T_node)
-            T_node.update_score(g, h)
+            g, h = self.calculate_score(T)
+            T.update_score(g, h)
             # - If T is in the closed list: 
             #       Ignore it.
-            if T_node in self.closed:
-                continue
+            in_closed = False
+            for node in self.closed:
+                if node.pos() == T.pos():
+                    in_closed = True
+            if in_closed: continue
             
-            in_open = False
+            T_old = None
             for node in self.open:
-                if node.pos() == T_node.pos()
-                    in_open = True
+                if node.pos() == T.pos():
+                    T_old = node
             # - If T is not in the open list: 
             #       Add it and compute its score.
-            if not in_open:
+            if T_old is None:
                 self.open.append(T)
             # - If T is already in the open list: 
             #       Check if the F score is lower when we use the 
             #       current generated path to get there. 
             #       If it is, update its score and update its parent as well.
             else:
-                T_old = self.open
-
+                if T.f < T_old.f:
+                    self.open.remove(T_old)
+                    self.open.append(T.f)
+            # - If T is the end node:
+            if T.pos() == self.end:
+                self.end_path = T
 
     def get_nodes_around(self, node):
         """Returns a list of coordinates around (up, down, left, right) a node."""
@@ -104,16 +113,16 @@ class AStar:
 
         # Up
         if y-1 > 0 and self.level_map[y-1][x] is not self.WALL:
-            coords.append((x, y-1))
+            coords.append(Node(x, y-1, node))
         # Down
         if y+1 < len(self.level_map) and self.level_map[y+1][x] is not self.WALL:
-            coords.append((x, y+1))
+            coords.append(Node(x, y+1, node))
         # Left
         if x-1 > 0 and self.level_map[y][x-1] is not self.WALL:
-            coords.append((x-1, y))
+            coords.append(Node(x-1, y, node))
         # Right
-        if x+1 < len(self.level_map[0]) and self.level_map[y][x+1] is not self.WALL:
-            coords.append((x+1, y))
+        if x+1 < len(self.level_map[y]) and self.level_map[y][x+1] is not self.WALL:
+            coords.append(Node(x+1, y, node))
 
         return coords
 
@@ -128,6 +137,43 @@ class AStar:
 
         return g, h
 
+    def get_final_path(self):
+        """Returns an ordered list of Nodes leading from the start to the end."""
+        if self.end_path is not None:
+            node = self.end_path
+            path = [self.end_path]
+            
+            while node.parent is not None:
+                path = [node.parent] + path
+                node = node.parent
+            
+            return path
+
+    def get_node_map(self):
+        """Returns a 2D list of Nodes representing the map."""
+        node_map = self.level_map[:]
+        for y, row in enumerate(node_map):
+            for x, point in enumerate(row):
+                # If in the open list
+                in_open = None
+                for node in self.open:
+                    if node.pos() == (x, y):
+                        in_open = node
+                if in_open is not None:
+                    node_map[y][x] = ("OPEN", in_open)
+
+                # If in the closed list
+                in_closed = None
+                for node in self.closed:
+                    if node.pos() == (x, y):
+                        in_closed = node
+                if in_closed is not None:
+                    node_map[y][x] = ("CLOSED", in_closed)
+
+                # If in neither
+                if not in_open and not in_closed:
+                    node_map[y][x] = None
+        return node_map
 
 class Node:
     """A node on the map"""
@@ -144,7 +190,7 @@ class Node:
         return (self.x, self.y)
 
     def __str__(self):
-        return "Node(" + x + ", " + y + "): F=" + self.f + " G=" + self.g + " H=" + self.h
+        return "Node(" + str(self.x) + ", " + str(self.y) + "): F=" + str(self.f) + " G=" + str(self.g) + " H=" + str(self.h)
 
 
 class InvalidStartError(Exception):
--- visualization.py
+++ visualization.py
@@ -11,12 +11,17 @@ class Visualization(Canvas):
     """The canvas on which a visualization of the A* Algorithm is drawn on."""
 
     def __init__(self, master, **options):
+        # Initialize canvas
         Canvas.__init__(self, master, **options) # Initialize parent class
-        self.pack()
+        self.grid(row=0, column=0)
 
-        self.last_update = current_time_millis()
+        self.last_update = current_time_millis() # FPS
 
-        print("Canvas initialized...")
+        # Control variables
+        self.x_offset, self.y_offset = 0, 0
+        self.scale = 1
+
+        self.node_map = None
 
     def tick(self):
         if current_time_millis() - self.last_update >= 1000/CANVAS_FPS:
@@ -30,3 +35,10 @@ class Visualization(Canvas):
     def draw(self):
         self.delete(ALL) # Delete all objects from the previous draw loop
         self.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, fill="white", width=0) # Clear the screen
+
+        # Draw the nodes
+        node_width = 50 * self.scale
+        for y, row in enumerate(self.node_map):
+            for x, node in enumerate(row):
+                self.create_rectangle(x*node_width+self.x_offset, y*node_width+self.y_offset, node_width, node_width, 
+                                      fill='white', outline='black', width=1)
