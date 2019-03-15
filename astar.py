484a33032e8ebe9d3d19ddd1ff40982b61c5c1bb
Connor M. C <42192146+gestalt8003@users.noreply.github.com>
2019-03-14 21:24:18 -0500

    Update algo

--- astar.py
+++ astar.py
@@ -2,12 +2,12 @@
 
 
 
-
+get_distance_to_end = lambda node, end: abs(end[0] - node.x) + abs(end[1] - node.y)
 
 class AStar:
     """The A* search Algorithm."""
 
-    # Level map point constants
+    # Level map node constants
     EMPTY = 0
     WALL = 1
     START = 2
@@ -15,98 +15,136 @@ class AStar:
 
     def __init__(self, level_map):
         self.level_map = level_map
-        # Find the start/end points
+        # Find the start/end nodes
         self.start, self.end = self.read_level_map(self.level_map)
 
-        print("Start point found:", self.start)
-        print("End point found:", self.end)
+        print("Start node found:", self.start)
+        print("End node found:", self.end)
+
+        # Initialize start node
+        self.start_node = Node(self.start[0], self.start[1])
+        self.start_node.g = 0
+        self.start_node.h = get_distance_to_end(self.start_node, self.end)
 
         # Initialize the open/closed lists
-        self.open = self.get_points_around(self.start)
-        self.closed = [self.start]
+        self.open = self.get_nodes_around(self.start)
+        self.closed = [self.start_node]
 
-        # Initialize the search_points list, which contains F and parent points
-        self.search_points = []
-
-        print(self.open)
         self.advance()
 
 
     def read_level_map(self, level_map):
-        """Reads a 2D array and returns the start and end points.
-           Raises an error if there are no start or end points."""
+        """Reads a 2D array and returns the start and end nodes.
+           Raises an error if there are no start or end nodes."""
         start = None
         end = None
         
-        # Find the start/end points
+        # Find the start/end nodes
         for y, row in enumerate(level_map):
-            for x, point in enumerate(row):
-                if point == self.START:
+            for x, node in enumerate(row):
+                if node == self.START:
                     if start is None:
                         start = (x, y)
                     else:
-                        raise InvalidStartError("There are multiple start points on the map.")
-                elif point == self.END:
+                        raise InvalidStartError("There are multiple start nodes on the map.")
+                elif node == self.END:
                     if end is None:
                         end = (x, y)
                     else:
-                        raise InvalidEndError("There are multiple end points on the map.")
+                        raise InvalidEndError("There are multiple end nodes on the map.")
         
-        # Check if there any start/end points were found.
+        # Check if there any start/end nodes were found.
         if start is None:
-            raise InvalidStartError("There is no start point on the map.")
+            raise InvalidStartError("There is no start node on the map.")
         if end is None:
-            raise InvalidEndError("There is no end point on the map.")
+            raise InvalidEndError("There is no end node on the map.")
 
         return start, end
 
     def advance(self):
         """Advances the search."""
-        # Get the point on the open list which has the lowest score.
-        s = self.calculate_score((0, 0))
+        # Get the node on the open list which has the lowest score (S).
+        S = self.open[0]
+        for node in self.open:
+            if node.f < S.f:
+                S = node
+        # Remove S from the open list and add it to the closed list
+        self.open.remove(S)
+        self.closed.append(S)
+        # For each node T in S’s walkable adjacent tiles: 
+        for T in self.get_nodes_around(S):
+            T_node = Node(T[0], T[1], parent=S)
+            g, h = self.calculate_score(T_node)
+            T_node.update_score(g, h)
+            # - If T is in the closed list: 
+            #       Ignore it.
+            if T_node in self.closed:
+                continue
+            
+            in_open = False
+            for node in self.open:
+                if node.pos() == T_node.pos()
+                    in_open = True
+            # - If T is not in the open list: 
+            #       Add it and compute its score.
+            if not in_open:
+                self.open.append(T)
+            # - If T is already in the open list: 
+            #       Check if the F score is lower when we use the 
+            #       current generated path to get there. 
+            #       If it is, update its score and update its parent as well.
+            else:
+                T_old = self.open
 
-    def get_points_around(self, point):
-        """Returns a list of points around (up, down, left, right) a Point."""
-        x, y = point.x, point.y
-        points = []
+
+    def get_nodes_around(self, node):
+        """Returns a list of coordinates around (up, down, left, right) a node."""
+        x, y = node.x, node.y
+        coords = []
 
         # Up
-        if y-1 > 0 and self.level_map[y-1][x] is not None:
-            points.append((x, y-1))
+        if y-1 > 0 and self.level_map[y-1][x] is not self.WALL:
+            coords.append((x, y-1))
         # Down
-        if y+1 < len(self.level_map) and self.level_map[y+1][x] is not None:
-            points.append((x, y+1))
+        if y+1 < len(self.level_map) and self.level_map[y+1][x] is not self.WALL:
+            coords.append((x, y+1))
         # Left
-        if x-1 > 0 and self.level_map[y][x-1] is not None:
-            points.append((x-1, y))
+        if x-1 > 0 and self.level_map[y][x-1] is not self.WALL:
+            coords.append((x-1, y))
         # Right
-        if x+1 < len(self.level_map[0]) and self.level_map[y][x+1] is not None:
-            points.append((x+1, y))
+        if x+1 < len(self.level_map[0]) and self.level_map[y][x+1] is not self.WALL:
+            coords.append((x+1, y))
 
-        return points
+        return coords
 
-    def calculate_score(self, point):
-        """Calculates F for a point."""
-        print(point)
-        # Calculate G: the movement cost from the start point to the current point
-        g = point.parent.g + 1
+    def calculate_score(self, node):
+        """Calculates and returns G, H for a node."""
+        # Calculate G: the movement cost from the start node to the current node
+        g = node.parent.g + 1
 
-        # Calculate H: the estimated movement cost from the current point to the end point
+        # Calculate H: the estimated movement cost from the current node to the end node
         # Manhattan distance method: # of horizontal + # of vertical squares to reach the end
-        h = abs(self.end[0] - point[0]) + abs(self.end[1] - point[1])
+        h = get_distance_to_end(node, self.end)
 
-        return g + h
+        return g, h
 
 
-class Point:
+class Node:
+    """A node on the map"""
 
-    def __init__(self, x, y, g, h, parent=None):
+    def __init__(self, x, y, parent=None):
         self.x, self.y = x, y
-        self.f, self.g, self.h = g+h, g, h
+        self.f, self.g, self.h = 0, 0, 0
         self.parent = parent
 
     def update_score(self, g, h):
         self.f, self.g, self.h = g+h, g, h
+
+    def pos(self):
+        return (self.x, self.y)
+
+    def __str__(self):
+        return "Node(" + x + ", " + y + "): F=" + self.f + " G=" + self.g + " H=" + self.h
 
 
 class InvalidStartError(Exception):
